5e051ede22b5912cae694cf4877445b261ce074b
Andrew Tranquada <drewski@drewstud.com>
2015-11-02 15:07:56 -0500

    Update configure_nginx.rb

--- recipes/configure_nginx.rb
+++ recipes/configure_nginx.rb
@@ -56,4 +56,10 @@ service 'php-fpm' do
   action [:start, :enable]
 end
 
+ execute 'fix_selinux_template_smarty' do
+  command "semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/postfixadmin/templates_c'" 
+end
 
+execute 'restorecon template'do
+  command "restorecon -v '/var/www/html/postfixadmin/templates_c'"
+end
