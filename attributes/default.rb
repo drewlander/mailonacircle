c5345b745dc58d17bd0ff79262c9ff513818b24e
Andrew Tranquada <drewski@drewstud.com>
2015-11-02 22:14:58 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,5 +13,5 @@ default['postfixadmin']['postfixadmin_password'] = 'abc123'
 default['postfixadmin']['md5crypt_pw'] = '$1$y17yIJD1$DgFXoo1LGKWr8446FIe4q0'
 default['postfixadmin']['postfixadmin_download_url'] = 'http://downloads.sourceforge.net/project/postfixadmin/postfixadmin/postfixadmin-2.93/postfixadmin-2.93.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fpostfixadmin%2Ffiles%2F&ts=1446337172&use_mirror=iweb'
 
-default['nginx']['postfixadmin_port'] = '80'
+default['nginx']['postfixadmin_port'] = '443'
 default['nginx']['postfixadmin_servername'] = 'mail.test.com'
